917d71d6628596281aea95855f0751424fa439c1
Sharat Bhat <Sharat.Bhat@bizdata.com.au>
2020-12-23 20:37:31 +1100

    Remove Author from comment

--- HakkuPaysaAPI/DTOs/Comment.cs
+++ HakkuPaysaAPI/DTOs/Comment.cs
@@ -9,8 +9,7 @@ namespace HakkuPaysaAPI.DTOs
     {
         public Guid PostId { get; set; }
         public Guid CommentId { get; set; }
-        public Guid AuthorId { get; set; }
-        public string Author { get; set; }
+        public string AuthorId { get; set; }
         public string Text { get; set; }
         public DateTime CommentedOn { get; set; }
         public DateTime UpdatedOn { get; set; }

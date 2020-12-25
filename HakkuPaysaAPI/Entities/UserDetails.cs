c840bac55464df879eb5c66d354cc18e529031ba
Sharat Bhat <sharattbhat@gmail.com>
2020-12-25 12:50:05 +1100

    Create UserDetails entity

---
+++ HakkuPaysaAPI/Entities/UserDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HakkuPaysaAPI.Entities
+{
+    public class UserDetails
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string ProfilePic { get; set; }
+        public string Status { get; set; }
+        public DateTime DOB { get; set; }
+        public string Intro { get; set; }
+        public Address Address { get; set; }
+        public Contact Contact { get; set; }
+    }
+}

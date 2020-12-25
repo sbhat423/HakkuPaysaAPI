dd4b699627130d4b57283b752cabc628e9678e43
Sharat Bhat <sharattbhat@gmail.com>
2020-12-25 12:48:41 +1100

    Create Contact Entity

---
+++ HakkuPaysaAPI/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HakkuPaysaAPI.Entities
+{
+    public class Contact
+    {
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string FBAccountLink { get; set; }
+        public string InstaAccountLink { get; set; }
+        public string LinkeInAccountLink { get; set; }
+    }
+}

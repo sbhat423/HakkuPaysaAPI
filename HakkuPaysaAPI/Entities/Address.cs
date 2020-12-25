4b19007112ef7355f322ad76d10677d7850d6b69
Sharat Bhat <sharattbhat@gmail.com>
2020-12-25 12:44:43 +1100

    Create Address Entity

---
+++ HakkuPaysaAPI/Entities/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HakkuPaysaAPI.Entities
+{
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+    }
+}
